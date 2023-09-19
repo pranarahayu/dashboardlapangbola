8164560fa19c97e5e925c3b06951bde5ce675376
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2023-09-19 16:28:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -72,30 +72,30 @@ with tab1:
             if all_teams:
                 if (season == 'All Season'):
                     with col1:
-                        st.metric(label="Goals", value=histodata['Goal'].sum()
+                        st.metric(label="Goals", value=histodata['Goal'].sum())
                     with col2:
-                        st.metric(label="Assists", value=histodata['Assist'].sum()
+                        st.metric(label="Assists", value=histodata['Assist'].sum())
                     with col3:
-                        st.metric(label="Yellow Cards", value=histodata['Yellow Card'].sum()
+                        st.metric(label="Yellow Cards", value=histodata['Yellow Card'].sum())
                     with col4:
-                        st.metric(label="Red Cards", value=histodata['Red Card'].sum()
+                        st.metric(label="Red Cards", value=histodata['Red Card'].sum())
                     with col5:
-                        st.metric(label="Concededs", value=histodata['Conceded'].sum()
+                        st.metric(label="Concededs", value=histodata['Conceded'].sum())
                     with col6:
-                        st.metric(label="Clean Sheets", value=histodata['Clean Sheet'].sum()
+                        st.metric(label="Clean Sheets", value=histodata['Clean Sheet'].sum())
                 else:
                     with col1:
-                        st.metric(label="Goals", value=curdata2['Goal'].sum()
+                        st.metric(label="Goals", value=curdata2['Goal'].sum())
                     with col2:
-                        st.metric(label="Assists", value=curdata2['Assist'].sum()
+                        st.metric(label="Assists", value=curdata2['Assist'].sum())
                     with col3:
-                        st.metric(label="Yellow Cards", value=curdata2['Yellow Card'].sum()
+                        st.metric(label="Yellow Cards", value=curdata2['Yellow Card'].sum())
                     with col4:
-                        st.metric(label="Red Cards", value=curdata2['Red Card'].sum()
+                        st.metric(label="Red Cards", value=curdata2['Red Card'].sum())
                     with col5:
-                        st.metric(label="Concededs", value=curdata2['Conceded'].sum()
+                        st.metric(label="Concededs", value=curdata2['Conceded'].sum())
                     with col6:
-                        st.metric(label="Clean Sheets", value=curdata2['Clean Sheet'].sum()
+                        st.metric(label="Clean Sheets", value=curdata2['Clean Sheet'].sum())
             else:
                 if (season == 'All Season'):
                     with col1:
