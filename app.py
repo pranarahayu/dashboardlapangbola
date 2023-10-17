9ded92c4f6de1abc0086433b56a8007554db3b11
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2023-10-17 16:42:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,6 @@ from assignxg import get_wdl
 from assignxg import get_skuad
 from assignxg import get_formasi
 from assignxg import get_radar
-from assignxg import get_simi
 
 sys.path.append("fungsiplot.py")
 import fungsiplot
@@ -308,9 +307,6 @@ with tab3:
             st.subheader(ply+' Scouting Report')
             st.caption('vs '+pos+' in '+komp+' | Min. '+str(mins)+' mins played')
             st.data_editor(rdr, column_config={'Percentile':st.column_config.ProgressColumn('Percentile',width='medium',min_value=0,max_value=1)},hide_index=True)
-        with col6:
-            smr = get_simi(rank_p90,df2,ply,pos)
-            st.dataframe(smr.head(5), hide_index=True)
     with plo:
         col1, col2, col3, col4 = st.columns(4)
         with col1:
