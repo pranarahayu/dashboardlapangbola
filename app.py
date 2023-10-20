d7055189d694494573c1040236960c9a6c865c3e
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2023-10-20 10:50:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -333,8 +333,7 @@ with tab3:
             st.subheader(ply+' Scouting Report')
             st.caption('vs '+pos+' in '+komp+' | Min. '+str(mins)+' mins played')
             st.data_editor(rdr, column_config={'Percentile':st.column_config.ProgressColumn('Percentile',width='medium',min_value=0,max_value=1)},hide_index=True)
-            piz = beli_pizza(komp, pos, klub, ply, rank_pct, mins)
-            @st.cache
+            beli_pizza(komp, pos, klub, ply, rank_pct, mins)
             with open('pizza.jpg', 'rb') as img:
                 fn = 'Perf.Radar_'+ply+'.jpg'
                 btn = st.download_button(label="Download Report as a Radar!", data=img,
